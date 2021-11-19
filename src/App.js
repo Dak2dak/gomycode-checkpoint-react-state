5a28ceb17f2ef13d3f45dd3ec0b1e72bd4ec9f3b
Didier Dakoury <dazibao76@gmail.com>
2021-11-19 16:10:20 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './Style.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+
+  };
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
